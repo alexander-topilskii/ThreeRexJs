ab371f99bc0e73406d5c868837f681d0521ec0da
alexander.topilskii <alexander.topilskii@gmail.com>
2025-10-29 10:41:50 +0200

    add ux

--- samples/ux/ux-controls-1/src/main.ts
+++ samples/ux/ux-controls-1/src/main.ts
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import Split from 'split.js'
+
+Split(['#left', '#right'])
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
